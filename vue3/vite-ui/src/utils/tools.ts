67231508668b201bc4fa261843666016375c378d
kw <932223023@qq.com>
2021-12-30 17:48:37 +0800

    增加工具方法

--- vue3/vite-ui/src/utils/tools.ts
+++ vue3/vite-ui/src/utils/tools.ts
@@ -2,6 +2,7 @@ import {globalConfig} from "./config";
 import dayjs from 'dayjs'
 import {isNull} from "./basicTools";
 import placeholder from "lodash/fp/placeholder";
+import {keyOf} from "element-plus/es/utils/props";
 // 处理图片路径
 export const getPicturePath = (str:string) => {
   if (str.startsWith('http')) {
@@ -52,25 +53,54 @@ export const setListValueLabel = (arr: Array<PlainObject>, config: ValueLabelCon
 }
 /*
 * 筛选列表,根据value的值,获取相应的展示label值
-* arr: 对象数组, 里面需要有value,label选项,这样才能获取到正确的值
-* value: 作为比对的值
-* placeholder: 占位符,默认 '-'
-* 返回值: 占位符或者任意值,一般是字符串
+* arr: 对象数组, 这个对象有什么属性,那么相应的返回值也有什么属性,并额外加上configkeys里面每一个属性值作为key的数据
+* value: 作为比对的值,先只用字符串 (可以为数字,字符串, 或者字符串,数字数组),
+* configkeys: {}, 一个对象, value, label 配置必须存在 ,配置需要添加或者转换的key, 默认值为{value: 'value', label: 'label'}, 但还可以添加其他属性
+* placeholder: 占位符,默认 '-', 字符串
+* 返回值: 返回跟数组对象一样的类型, 并新添加属性: configkeys里面的每一个属性值
 * */
 type StringAndNumber = string | number
-interface FilterEnumsParams {
-  list: Array<PlainObject>
-  value: StringAndNumber | StringAndNumber[]
-  config:  ValueLabelConfig
-  placeholder: string
+interface FilterEnumsParams<T>{
+  list: Array<T>
+  value: string
+  configKeys?:  ValueLabelConfig
+  placeholder?: string
 }
 
-export const filterEnums = (arr: Array<PlainObject>, value:any, placeholder?:string): any{
-  placeholder = placeholder || '-'
-  if(arr.length === 0 || isNull(value)) {
-    return placeholder
+type TurnKeys <T> =  T[keyof T]
+
+type UnionValueLabelObject<T,U> = Record<keyof T | keyof U, T[keyof T]>
+  // [p in T | U)]: T[p] | T[U[P]],
+  // [p in keyof T]: T[p]
+  // [k in keyOf U]: T[keyOf TurnKeys<U>]
+
+
+export const filterEnums = <T extends {[propName: string]: any}>(params: FilterEnumsParams<T>): T&ValueLabelConfig => {
+  const {list = [], value = '', configKeys = {value: 'value', label: 'label'}, placeholder = '-'} = params
+  const keys = Object.keys(configKeys)
+  let defaultResult = Object.create(null)
+  if(list.length === 0 || isNull(value)) {
+    keys.forEach(key => {
+      defaultResult[key] = placeholder
+    })
+    return defaultResult
   } else {
-    const tempObj = arr.find(i => i.value === value!) || {}
-    return tempObj.label || placeholder
+    const valueKey = configKeys.value
+    const index = list.findIndex((i:T) => {
+      const flag = i[valueKey] === value
+      if(flag) {
+        defaultResult = {...i}
+        keys.forEach((key:string) => {
+          defaultResult[key] = i[configKeys[key]] || placeholder
+        })
+      }
+      return flag
+    })
+    if(index === -1) {
+      keys.forEach(key => {
+        defaultResult[key] = placeholder
+      })
+    }
+    return defaultResult
   }
 }
